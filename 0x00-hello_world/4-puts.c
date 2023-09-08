5c1d66426946e221f6580c425860f05300a0e7a8
Shengero4 <sdauceedouglas@gmail.com>
2023-09-07 17:22:28 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programm that prints a line with pus function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
