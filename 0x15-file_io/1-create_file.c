54b95ca4508f4c8f6649c443a10c869c4142aa15
Shengero4 <sdauceedouglas@gmail.com>
2023-10-31 03:36:13 +0200

    betty kept showing mistakes which i could not understand the origin so i decided to restart hope fully it works

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file_descriptor == -1)
 		return (-1);
 
@@ -26,12 +26,15 @@ int create_file(const char *filename, char *text_content)
 		while (text_content[length] != '\0')
 			length++;
 
+		bytes_written = write(file_descriptor, text_content, length);
 		if (bytes_written == -1 || bytes_written != length)
 		{
-
+			close(file_descriptor);
 			return (-1);
 		}
 	}
 
+	close(file_descriptor);
 	return (1);
 }
+
