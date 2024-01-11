23a82e016561be1c487e429fb861d04adeb34cb5
Shengero4 <sdauceedouglas@gmail.com>
2024-01-11 17:45:25 +0200

    using puts to print a string instead of printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void)
+{
+	char text[] = ""Programming is like building a multilingual puzzle";
+	// using puts to print a string instead of printf
+	puts(text);
+	return(0);
+}
