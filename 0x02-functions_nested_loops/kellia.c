f7fd20cefe9c2647713833ce364fc93bc77bec9e
Shengero4 <sdauceedouglas@gmail.com>
2024-02-27 21:16:07 +0200

    pushing the main file of thfor the current projectw

--- 0x02-functions_nested_loops/kellia.c
+++ 0x02-functions_nested_loops/kellia.c
@@ -1,33 +1,25 @@
 #include <stdio.h>
- 
-/* function declaration */
-int max(int num1, int num2);
- 
-int main () {
 
-   /* local variable definition */
-   int a = 100;
-   int b = 200;
-   int ret;
- 
-   /* calling a function to get max value */
-   ret = max(a, b);
- 
-   printf( "Max value is : %d\n", ret );
- 
-   return 0;
+/**
+ * main - Entry point
+ * Description: this is a random program
+ */
+
+int factorial(int n)
+{
+	int retval = 1;
+	while(n! = 1) {
+		retval *= n--;
+	}
+	return retval;
 }
- 
-/* function returning the max between two numbers */
-int max(int num1, int num2) {
 
-   /* local variable declaration */
-   int result;
- 
-   if (num1 > num2)
-      result = num1;
-   else
-      result = num2;
- 
-   return result; 
+int main(void)
+{
+	int val = factorial(5);
+	Printf("%d! = %d\n",5,val);
+
+	printf("%d! = %d\n",4,factorial(4));
+	
+	return 0;
 }
